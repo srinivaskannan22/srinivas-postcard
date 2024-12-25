ea2aef29b19c5ec6d4f2475e9e4b2bb06faf3ebf
srinivas kannan <srinivaskannan22@gmail.com>
2024-12-25 22:29:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const a=document.getElementsByClassName("about");
+const b=document.getElementsByClassName("experience");
+const c=document.getElementsByClassName("contact");
+
+a.style.display=none;
+b.display=none;
+c.display=none;
+
+function about(){
+        const b=document.getElementsByClassName("experience");
+        const c=document.getElementsByClassName("contact");
+        b.style.display="none";
+        c.style.display="none";
+
+}
